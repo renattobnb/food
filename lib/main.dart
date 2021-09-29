9a0dc9eed39cfcd2c02b4792807ac22edfb57c0d
Renato <renattobnb@gmail.com>
2021-09-28 22:28:20 -0300

    Criação da tela principal escolha dos restaurantes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:appfood/model/restaurante_model.dart';
 import 'package:appfood/view_model/main_view_model_imp.dart';
+import 'package:appfood/widgtes/common/common_widgets.dart';
+import 'package:auto_animated/auto_animated.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -55,11 +58,66 @@ class MyHomePage extends StatelessWidget {
             );
           else {
             var lst = snapshot.data as List<RestauranteModel>;
-            return ListView.builder(
-              itemCount: lst.length,
-              itemBuilder: (context, index) {
-                return Text(lst[index].nome);
-              },
+            return Container(
+              margin: const EdgeInsets.only(top: 10),
+              child: LiveList(
+                showItemDuration: Duration(milliseconds: 350),
+                showItemInterval: Duration(milliseconds: 150),
+                reAnimateOnVisibility: true,
+                scrollDirection: Axis.vertical,
+                itemCount: lst.length,
+                itemBuilder: animationItemBuilder((index) => InkWell(
+                      onTap: () {},
+                      child: Container(
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height / 2.5 * 1.1,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 250,
+                              child: Card(
+                                semanticContainer: true,
+                                clipBehavior: Clip.antiAliasWithSaveLayer,
+                                child: CachedNetworkImage(
+                                    imageUrl: lst[index].imageUrl,
+                                    fit: BoxFit.cover,
+                                    errorWidget: (context, url, err) => Center(
+                                          child: Icon(Icons.image),
+                                        ),
+                                    progressIndicatorBuilder: (context, url,
+                                            donwloadProgress) =>
+                                        Center(
+                                          child: CircularProgressIndicator(),
+                                        )),
+                              ),
+                            ),
+                            Card(
+                              child: Container(
+                                padding: const EdgeInsets.all(8),
+                                width: double.infinity,
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      lst[index].nome,
+                                      style: GoogleFonts.jetBrainsMono(
+                                          fontWeight: FontWeight.w900),
+                                    ),
+                                    Text(
+                                      lst[index].endereco,
+                                      style: GoogleFonts.jetBrainsMono(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 12),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )),
+              ),
             );
           }
         },
